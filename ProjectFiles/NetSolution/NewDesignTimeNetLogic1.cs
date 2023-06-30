e0ca4887425e4e4eaf0dae80c236853074827752
AzuraZhang <118424349+AzuraZhang@users.noreply.github.com>
2023-06-30 15:41:22 +0800

    Create NewDesignTimeNetLogic1.cs

---
+++ ProjectFiles/NetSolution/NewDesignTimeNetLogic1.cs
@@ -0,0 +1,22 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.UI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.OPCUAServer;
+#endregion
+
+public class NewDesignTimeNetLogic1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method Remote Change
+    }
+}
